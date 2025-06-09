dcd8694480cf9afd5f41dd0c2d4b82d29bb5558a
sitimasda15 <mazdasiti15@gmail.com@domain.com>
2025-06-09 19:06:12 +0700

    membuat scene 12

--- src/scenes/scene_12/scene_12.cpp
+++ src/scenes/scene_12/scene_12.cpp
@@ -1,19 +1,43 @@
 #include "scene_12.hpp"
 #include <iostream>
 
-void Scene12_DialogScene12(Scene *scene)
+void Scene12_DialogPilihan1(Scene *scene)
 {
-    std::cout << "Dialog beres\n";
+    std:: cout << "Memilih pilihan 1\n";
     Scene *scene13 = Scene13_Create();
-    // // SceneManager_GoToScene(scene->sceneManager, scene13);
+    SceneManager_GoToScene(scene->sceneManager, scene13, SceneTransition::Fade);
+}
+
+void Scene12_DialogPilihan2(Scene *scene)
+{
+    std:: cout << "Memilih pilihan 2\n";
+    Scene *scene14 = Scene14_Create();
+    SceneManager_GoToScene(scene->sceneManager, scene14, SceneTransition::Fade);
 }
 
 void Scene12_Start(Scene *scene)
 {
     Scene12Data *data = (Scene12Data *)scene->data;
-    SceneManager_SetBackground(scene->sceneManager, "tiba_dikota.png");
-    // SceneManager_AddDialog(scene->sceneManager, true, "MC","Aku harus cepat! Kalau telat, mereka bisa langsung mencoret namaku..", "talkingWork.png", Scene12_DialogScene12, scene);
-
+    SceneManager_SetBackground(scene->sceneManager, "background/interview.png");
+    SceneManager_AddDialog(
+        scene->sceneManager,
+        {
+            DialogPerson{
+                .imageFilePath = "figuran/katakTertawa.png",
+                .position = DialogPersonPosition::Right,
+                .animation = DialogPersonAnimation::Slide},
+            },
+            {
+                DialogQuestion{
+                    .question = "Terimakasih dan duduk tegap",
+                    .onAnswered = Scene12_DialogPilihan1,
+                    .onAnsweredParameter = scene},
+                DialogQuestion{
+                    .question = "Mbeeeee (dengan nada pelan)",
+                    .onAnswered = Scene12_DialogPilihan2,
+                    .onAnsweredParameter = scene}},
+                    "Mr.Katak",
+                    "Kreatif dengan fonetik... saya suka! ");
     SceneManager_PlayMusic(scene->sceneManager, "Morning.mp3");
 }
 
--- src/scenes/scene_12/scene_12.hpp
+++ src/scenes/scene_12/scene_12.hpp
@@ -3,6 +3,7 @@
 #include "SFML/Audio.hpp"
 #include "../scene.hpp"
 #include "../scene_13/scene_13.hpp"
+#include "../scene_14/scene_14.hpp"
 #include "../../umum.hpp"
 
 struct Scene12Data
