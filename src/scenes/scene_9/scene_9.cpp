acf6eb456ffdf1768651623d8cd8dd19f5d2e619
sitimasda15 <mazdasiti15@gmail.com@domain.com>
2025-06-09 12:33:55 +0700

    membuat scene 9

--- src/scenes/scene_9/scene_9.cpp
+++ src/scenes/scene_9/scene_9.cpp
@@ -32,30 +32,41 @@ void Scene9_DialogPilihan4(Scene *scene)
 void Scene9_Start(Scene *scene)
 {
     Scene9Data *data = (Scene9Data *)scene->data;
-    SceneManager_SetBackground(scene->sceneManager, "background/")
-}
-
-
-
-void Scene9_DialogScene9(Scene *scene)
-{
-    std::cout << "Dialog beres\n";
-    Scene *scene10 = Scene10_Create();
-    // SceneManager_GoToScene(scene->sceneManager, scene10);
-}
-
-void Scene9_Start(Scene *scene)
-{
-    Scene9Data *data = (Scene9Data *)scene->data;
-    SceneManager_SetBackground(scene->sceneManager, "tiba_dikota.png");
-    // SceneManager_AddDialog(scene->sceneManager, true, "MC","Aku harus cepat! Kalau telat, mereka bisa langsung mencoret namaku..", "talkingWork.png", Scene9_DialogScene9, scene);
+    SceneManager_SetBackground(scene->sceneManager, "background/interview.png");
+    SceneManager_AddDialog(
+        scene->sceneManager,
+        {
+            DialogPerson{
+                .imageFilePath = "HRD/badutBicara.png",
+                .position = DialogPersonPosition::Right,
+                .animation = DialogPersonAnimation::None},
+            },
+            {
+                DialogQuestion{
+                    .question = "Baaaa-drun Syah",
+                    .onAnswered = Scene9_DialogPilihan1,
+                    .onAnsweredParameter = scene},
+                DialogQuestion{
+                    .question = "Kambing bin Ternak",
+                    .onAnswered = Scene9_DialogPilihan2,
+                    .onAnsweredParameter = scene},
+                DialogQuestion{
+                    .question = "Mengembik ",
+                    .onAnswered = Scene9_DialogPilihan3,
+                    .onAnsweredParameter = scene},
+                DialogQuestion{
+                    .question = "Tulis nama di kertas",
+                    .onAnswered = Scene9_DialogPilihan4,
+                    .onAnsweredParameter = scene}},
+                "Dr. Badut",
+                "Pertama… kalau kamu seekor kambing, apa nama lengkapmu, mbee?");
 
     SceneManager_PlayMusic(scene->sceneManager, "Morning.mp3");
 }
 
 void Scene9_Update(Scene *scene)
 {
-    Scene9Data *data = (Scene9Data *)scene->data;
+    //Scene9Data *data = (Scene9Data *)scene->data;
 }
 
 void Scene9_Destroy(Scene *scene)
--- src/scenes/scene_9/scene_9.hpp
+++ src/scenes/scene_9/scene_9.hpp
@@ -2,7 +2,10 @@
 
 #include "SFML/Audio.hpp"
 #include "../scene.hpp"
-#include "../scene_10/scene_10.hpp"
+#include "../scene_12/scene_12.hpp"
+#include "../scene_13/scene_13.hpp"
+#include "../scene_14/scene_14.hpp"
+#include "../scene_15/scene_15.hpp"
 #include "../../umum.hpp"
 
 struct Scene9Data
