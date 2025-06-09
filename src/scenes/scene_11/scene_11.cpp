48cef5b2fafd08f7101080b4c7e89d8241ac5a3f
sitimasda15 <mazdasiti15@gmail.com@domain.com>
2025-06-09 18:36:15 +0700

    membuat scene 11

--- src/scenes/scene_11/scene_11.cpp
+++ src/scenes/scene_11/scene_11.cpp
@@ -1,25 +1,61 @@
 #include "scene_11.hpp"
 #include <iostream>
 
-void Scene11_DialogScene11(Scene *scene)
+void Scene11_DialogPilihan1(Scene *scene)
 {
-    std::cout << "Dialog beres\n";
-    Scene *scene12 = Scene12_Create();
-    // // SceneManager_GoToScene(scene->sceneManager, scene12);
+    std:: cout << "Pilihan 1 dipilih\n";
+    Scene *scene17 = Scene17_Create();
+    SceneManager_GoToScene(scene->sceneManager, scene17, SceneTransition::Fade);
+}
+
+void Scene11_DialogPilihan2(Scene *scene)
+{
+    std:: cout << "Pilihan 2 dipilih\n";
+    Scene *scene18 =Scene18_Create();
+    SceneManager_GoToScene(scene->sceneManager, scene18, SceneTransition::Fade);
 }
 
 void Scene11_Start(Scene *scene)
 {
     Scene11Data *data = (Scene11Data *)scene->data;
-    SceneManager_SetBackground(scene->sceneManager, "tiba_dikota.png");
-    // SceneManager_AddDialog(scene->sceneManager, true, "MC","Aku harus cepat! Kalau telat, mereka bisa langsung mencoret namaku..", "talkingWork.png", Scene11_DialogScene11, scene);
+    SceneManager_SetBackground(scene->sceneManager, "background/interview.png");
+    SceneManager_AddDialog(
+            scene->sceneManager,
+        {
+            DialogPerson{
+                .imageFilePath = "work/confusedWork.png",
+                .position = DialogPersonPosition::Left,
+                .animation = DialogPersonAnimation::Slide},
+            },
+            {},
+            "MC",
+            "Ciieeeeetttsss…");
 
-    SceneManager_PlayMusic(scene->sceneManager, "Morning.mp3");
+    SceneManager_AddDialog(
+        scene->sceneManager,
+        {
+            DialogPerson{
+                .imageFilePath = "HRD/katakMeringis.png",
+                .position = DialogPersonPosition::Right,
+                .animation = DialogPersonAnimation::Slide},
+            },
+            {
+                DialogQuestion{
+                    .question = "Jawab dengan serius",
+                    .onAnswered = Scene11_DialogPilihan1,
+                    .onAnsweredParameter = scene},
+                DialogQuestion{
+                    .question = "Tertawa dan bercanda",
+                    .onAnswered = Scene11_DialogPilihan2,
+                    .onAnsweredParameter = scene}},
+                "Mr.Katak",
+                "Suara pintu kamu sangat realistis. Kamu mantan pintu?");
 }
+
 
 void Scene11_Update(Scene *scene)
 {
-    Scene11Data *data = (Scene11Data *)scene->data;
+    // Scene11Data *data = (Scene11Data *)scene->data;
 }
 
 void Scene11_Destroy(Scene *scene)
@@ -37,4 +73,3 @@ Scene *Scene11_Create()
     };
     return scene;
 }
-
