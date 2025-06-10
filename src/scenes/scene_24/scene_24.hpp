19c30ea134e10adde7826c3c5100a7ccb8f041a6
Fadhil Anwar <fadhilanwar42@gmail.com>
2025-06-10 10:15:40 +0700

    Membuat scene 24 (Tes Berikutnya)

--- src/scenes/scene_24/scene_24.hpp
+++ src/scenes/scene_24/scene_24.hpp
@@ -2,7 +2,10 @@
 
 #include "SFML/Audio.hpp"
 #include "../scene.hpp"
+#include "../scene_23/scene_23.hpp"
 #include "../scene_25/scene_25.hpp"
+#include "../scene_31/scene_31.hpp"
+#include "../scene_32/scene_32.hpp"
 #include "../../umum.hpp"
 
 struct Scene24Data
