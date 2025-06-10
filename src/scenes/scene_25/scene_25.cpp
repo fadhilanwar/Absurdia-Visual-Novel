44b84aa85e87a9c80ad3b841a4b8bbdb97364844
Fadhil Anwar <fadhilanwar42@gmail.com>
2025-06-10 10:16:00 +0700

    Membuat scene 25 (Teater Pintu)

--- src/scenes/scene_25/scene_25.cpp
+++ src/scenes/scene_25/scene_25.cpp
@@ -8,14 +8,94 @@ void Scene25_DialogScene25(Scene *scene)
     // // SceneManager_GoToScene(scene->sceneManager, scene26);
 }
 
+void playLaughSFX(Scene *scene) {
+    SceneManager_PlaySound(scene->sceneManager, "Crowd Laugh.wav");
+}
+
+void Scene25_PindahScene(Scene *scene)
+{
+    std::cout << "Pindah scene ke Scene 27 cenah\n";
+    Scene *Scene27 = Scene27_Create();
+    SceneManager_GoToScene(scene->sceneManager, Scene27, SceneTransition::Fade);
+
+    // SceneManager_PlaySound(scene->sceneManager, "Crowd Laugh.wav");
+
+}
+
 void Scene25_Start(Scene *scene)
 {
     Scene25Data *data = (Scene25Data *)scene->data;
-    SceneManager_SetBackground(scene->sceneManager, "tiba_dikota.png");
-    // SceneManager_AddDialog(scene->sceneManager, true, "MC","Aku harus cepat! Kalau telat, mereka bisa langsung mencoret namaku..", "talkingWork.png", Scene25_DialogScene25, scene);
 
+    SceneManager_SetBackground(scene->sceneManager, "background/interview.png");
+
+    SceneManager_AddDialog(
+        scene->sceneManager,
+        {
+            // DialogPerson{
+            //     .imageFilePath = "work/confusedWork.png",
+            //     .position = DialogPersonPosition::Left,
+            //     .animation = DialogPersonAnimation::None},
+        },
+        {},
+        "MC",
+        "(MC dan para interviewer berakting jadi pintu dan penghuni rumah.) ");
+    
+    SceneManager_AddDialog(
+        scene->sceneManager,
+        {
+            DialogPerson{
+                .imageFilePath = "HRD/katakBicara.png",
+                .position = DialogPersonPosition::Right,
+                .animation = DialogPersonAnimation::Slide},
+        },
+        {},
+        "Mr. Katak",
+        "Oh Oh! Aku yang jadi Kunci..!");
+
+    SceneManager_AddDialog(
+        scene->sceneManager,
+        {
+            DialogPerson{
+                .imageFilePath = "HRD/badutBicara.png",
+                .position = DialogPersonPosition::Right,
+                .animation = DialogPersonAnimation::Slide},
+        },
+        {},
+        "Dr. Badut",
+        "Kalau begitu, aku yang jadi Engsel..!");
+
+    SceneManager_AddDialog(
+        scene->sceneManager,
+        {
+            DialogPerson{
+                .imageFilePath = "HRD/pisangBicara.png",
+                .position = DialogPersonPosition::Right,
+                .animation = DialogPersonAnimation::Slide},
+        },
+        {},
+        "Pisang",
+        "PISAANGGG!", playLaughSFX, scene);
+
+    
+
+    SceneManager_AddDialog(
+        scene->sceneManager,
+        {
+            // DialogPerson{
+            //     .imageFilePath = "work/talkingWork.png",
+            //     .position = DialogPersonPosition::Left,
+            //     .animation = DialogPersonAnimation::None},
+        },
+        {},
+        "MC",
+        "(Mereka Tertawa dan Bertepuk Tangan..)",
+        Scene25_PindahScene, scene);
+
+    // Musik
     SceneManager_PlayMusic(scene->sceneManager, "Morning.mp3");
 }
+
+
 
 void Scene25_Update(Scene *scene)
 {
--- src/scenes/scene_25/scene_25.hpp
+++ src/scenes/scene_25/scene_25.hpp
@@ -3,6 +3,7 @@
 #include "SFML/Audio.hpp"
 #include "../scene.hpp"
 #include "../scene_26/scene_26.hpp"
+#include "../scene_27/scene_27.hpp"
 #include "../../umum.hpp"
 
 struct Scene25Data
