d887d5a52795cd29bd244250e3909f83fb8c1806
Fadhil Anwar <fadhilanwar42@gmail.com>
2025-06-10 10:12:37 +0700

    menambahkan ending b

---
+++ src/scenes/ending_b/ending_b.cpp
@@ -0,0 +1,34 @@
+#include "ending_b.hpp"
+#include <iostream>
+
+void Ending_b_Start(Scene *scene)
+{
+    Ending_bData *data = (Ending_bData *)scene->data;
+
+    // Set hanya background, tanpa karakter
+    SceneManager_SetBackground(scene->sceneManager, "endingB.png");
+
+    // Tampilkan pesan akhir tanpa karakter
+    SceneManager_AddDialog(
+        scene->sceneManager,
+        {}, // Tidak ada karakter
+        {},
+        "MC",
+        "Selamat! Kamu telah menyelesaikan game ini. Terima kasih telah bermain!"
+    );
+
+    // Mainkan musik ending
+    SceneManager_PlayMusic(scene->sceneManager, "Morning.mp3");
+}
+
+Scene* Ending_b_Create()
+{
+    Ending_bData *data = new Ending_bData();
+    Scene *scene = new Scene{
+        .data = data,
+        .start = Ending_b_Start,
+        .update = nullptr,   // Tidak ada update per frame
+        .destroy = nullptr   // Tidak ada resource tambahan
+    };
+    return scene;
+}
---
+++ src/scenes/ending_b/ending_b.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <SFML/Audio.hpp>
+#include "../scene.hpp"
+#include "../scene_27/scene_27.hpp"
+#include "../../umum.hpp"
+
+struct Ending_bData
+{
+    float animProgress;
+};
+
+Scene *Ending_b_Create();
