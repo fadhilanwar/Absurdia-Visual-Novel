b9b9c81612fdc6853c54fc14b0cdb3b26b1c90f1
Fadhil Anwar <fadhilanwar42@gmail.com>
2025-06-10 10:17:06 +0700

    Membuat scene 31 (Lagu kebangsaan penuh semangat)

--- src/scenes/scene_31/scene_31.cpp
+++ src/scenes/scene_31/scene_31.cpp
@@ -8,13 +8,62 @@ void Scene31_DialogScene31(Scene *scene)
     // SceneManager_GoToScene(scene->sceneManager, scene32);
 }
 
+void playMusicSFX(Scene *scene) {
+    SceneManager_PlayMusic(scene->sceneManager, "Morning.mp3");
+}
+
+void Scene31_PindahScene(Scene *scene)
+{
+    std::cout << "ENDING D TERBUKAAA\n";
+    Scene *ending_d = Ending_d_Create();
+    SceneManager_GoToScene(scene->sceneManager, ending_d, SceneTransition::Fade);
+
+    // SceneManager_PlaySound(scene->sceneManager, "Crowd Laugh.wav");
+
+}
+
 void Scene31_Start(Scene *scene)
 {
     Scene31Data *data = (Scene31Data *)scene->data;
-    SceneManager_SetBackground(scene->sceneManager, "tiba_dikota.png");
+    SceneManager_SetBackground(scene->sceneManager, "background/interview.png");
     // SceneManager_AddDialog(scene->sceneManager, true, "MC", "Aku harus cepat! Kalau telat, mereka bisa langsung mencoret namaku..", "talkingWork.png", Scene31_DialogScene31, scene);
 
-    SceneManager_PlayMusic(scene->sceneManager, "Morning.mp3");
+    SceneManager_PlaySound(scene->sceneManager, "anthem.mp3");
+
+
+    SceneManager_AddDialog(
+        scene->sceneManager,
+        {
+            DialogPerson{
+                .imageFilePath = "HRD/badutSenang.png",
+                .position = DialogPersonPosition::Left,
+                .animation = DialogPersonAnimation::Slide},
+            DialogPerson{
+                .imageFilePath = "HRD/KatakTertawa.png",
+                .position = DialogPersonPosition::Center,
+                .animation = DialogPersonAnimation::Slide},
+            DialogPerson{
+                .imageFilePath = "HRD/pisangBicara.png",
+                .position = DialogPersonPosition::Right,
+                .animation = DialogPersonAnimation::Slide},
+            },
+            
+        {},
+        "MC",
+        "(Panel berdiri dan ikut menyanyi. Pisang mengeluarkan konfeti dari topinya.) ", playMusicSFX, scene);
+
+        SceneManager_AddDialog(
+        scene->sceneManager,
+        {
+            DialogPerson{
+                .imageFilePath = "HRD/katakBicara.png",
+                .position = DialogPersonPosition::Right,
+                .animation = DialogPersonAnimation::Slide},
+            },
+        {},
+        "Mr. Katak",
+        "Dengan ini Selamat! Kamu jadi penyanyi utama acara tahunan kita!", Scene31_PindahScene, scene);
+
 }
 
 void Scene31_Update(Scene *scene)
--- src/scenes/scene_31/scene_31.hpp
+++ src/scenes/scene_31/scene_31.hpp
@@ -3,6 +3,7 @@
 #include "SFML/Audio.hpp"
 #include "../scene.hpp"
 #include "../scene_32/scene_32.hpp"
+#include "../ending_d/ending_d.hpp"
 #include "../../umum.hpp"
 
 struct Scene31Data
