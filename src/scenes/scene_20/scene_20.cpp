7cc3ae892a088dc86c7aec193e796a71d0a1365a
Fadhil Anwar <fadhilanwar42@gmail.com>
2025-06-10 10:14:31 +0700

    Membuat scene 20 (Suara Pintu Dramatis)

--- src/scenes/scene_20/scene_20.cpp
+++ src/scenes/scene_20/scene_20.cpp
@@ -8,11 +8,70 @@ void Scene20_DialogScene20(Scene *scene)
     // // SceneManager_GoToSceneene(scene->sceneManager, scene21);
 }
 
+void Scene20_DialogPilihan1(Scene *scene)
+{
+    std:: cout << "Pilihan 1 dipilih (Maen Peran Peranan) ";
+    SceneManager_AddDialog(
+        scene->sceneManager,
+        {
+            DialogPerson{
+                .imageFilePath = "work/talkingWork.png",
+                .position = DialogPersonPosition::Left,
+                .animation = DialogPersonAnimation::Slide},
+            },
+        {},
+        "Fuad Gemink",
+        "Kalau begitu, mari kita aktingkan ulang sejarah pintu pertama ditemukan.");
+    Scene *scene25 = Scene25_Create();
+    SceneManager_GoToScene(scene->sceneManager, scene25, SceneTransition::None);
+}
+
+void Scene20_DialogPilihan2(Scene *scene)
+{
+    std:: cout << "Pilihan 2 dipilih (Cerita Pintu) ";
+
+    SceneManager_AddDialog(
+        scene->sceneManager,
+        {
+            DialogPerson{
+                .imageFilePath = "work/talkingWork.png",
+                .position = DialogPersonPosition::Left,
+                .animation = DialogPersonAnimation::Slide},
+            },
+        {},
+        "Fuad Gemink",
+        "Saya pernah jadi pintu gudang tua. Banyak kenangan berdebu di sana...");
+
+
+    Scene *scene26 = Scene26_Create();
+    SceneManager_GoToScene(scene->sceneManager, scene26, SceneTransition::None);
+}
+
 void Scene20_Start(Scene *scene)
 {
     Scene20Data *data = (Scene20Data *)scene->data;
-    SceneManager_SetBackground(scene->sceneManager, "tiba_dikota.png");
+    SceneManager_SetBackground(scene->sceneManager, "background/interview.png");
     // SceneManager_AddDialog(scene->sceneManager, true, "MC","Aku harus cepat! Kalau telat, mereka bisa langsung mencoret namaku..", "talkingWork.png", Scene20_DialogScene20, scene);
+
+     SceneManager_AddDialog(
+        scene->sceneManager,
+        {
+            DialogPerson{
+                .imageFilePath = "HRD/katakTerpesona.png",
+                .position = DialogPersonPosition::Right,
+                .animation = DialogPersonAnimation::Slide},
+            },
+           {DialogQuestion{
+                .question = "Ajak Main Peran",
+                .onAnswered = Scene20_DialogPilihan1,
+                .onAnsweredParameter = scene},
+           {DialogQuestion{
+                .question = "Cerita Pengalaman jadi Pintu",
+                .onAnswered = Scene20_DialogPilihan2,
+                .onAnsweredParameter = scene}},
+           },
+        "Mr. Katak",
+        "(SERIUS) Ini sudah macam menonton sebuah Teater!");
 
     SceneManager_PlayMusic(scene->sceneManager, "Morning.mp3");
 }
--- src/scenes/scene_20/scene_20.hpp
+++ src/scenes/scene_20/scene_20.hpp
@@ -3,6 +3,8 @@
 #include "SFML/Audio.hpp"
 #include "../scene.hpp"
 #include "../scene_21/scene_21.hpp"
+#include "../scene_25/scene_25.hpp"
+#include "../scene_26/scene_26.hpp"
 #include "../../umum.hpp"
 
 struct Scene20Data
