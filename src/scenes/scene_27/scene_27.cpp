45819300194f6be19e3b4aae54d316c17250692c
Fadhil Anwar <fadhilanwar42@gmail.com>
2025-06-10 10:16:21 +0700

    Membuat scene 27 (Akhir Interview)

--- src/scenes/scene_27/scene_27.cpp
+++ src/scenes/scene_27/scene_27.cpp
@@ -8,11 +8,71 @@ void Scene27_DialogScene27(Scene *scene)
     // SceneManager_GoToScene(scene->sceneManager, scene28);
 }
 
+void Scene27_PindahScene(Scene *scene)
+{
+     std::cout << "ENDING B TERBUKAAA\n";
+    Scene *ending_b = Ending_b_Create();
+    SceneManager_GoToScene(scene->sceneManager, ending_b, SceneTransition::Fade);
+
+}
+
+void playApplauseSFX(Scene *scene) {
+    SceneManager_PlaySound(scene->sceneManager, "Applause.mp3");
+}
+
 void Scene27_Start(Scene *scene)
 {
     Scene27Data *data = (Scene27Data *)scene->data;
-    SceneManager_SetBackground(scene->sceneManager, "tiba_dikota.png");
-    // SceneManager_AddDialog(scene->sceneManager, true, "MC","Aku harus cepat! Kalau telat, mereka bisa langsung mencoret namaku..", "talkingWork.png", Scene27_DialogScene27, scene);
+    SceneManager_SetBackground(scene->sceneManager, "background/interview.png");
+
+
+    SceneManager_AddDialog(
+        scene->sceneManager,
+        {
+            DialogPerson{
+                .imageFilePath = "HRD/katakBicara.png",
+                .position = DialogPersonPosition::Right,
+                .animation = DialogPersonAnimation::Slide},
+        },
+        {},
+        "Mr. Katak",
+        "SELAMAATTT! Dengan ini kamu kami terima sebagai bagian dari Absurdia Inc.!", playApplauseSFX, scene);
+    
+    SceneManager_AddDialog(
+        scene->sceneManager,
+        {
+            DialogPerson{
+                .imageFilePath = "work/shockedWork.png",
+                .position = DialogPersonPosition::Left,
+                .animation = DialogPersonAnimation::Slide},
+        },
+        {},
+        "Fuad Gemink",
+        "Se-sserius, Pak? Saya… diterima?");
+
+    SceneManager_AddDialog(
+        scene->sceneManager,
+        {
+            DialogPerson{
+                .imageFilePath = "HRD/badutBicara.png",
+                .position = DialogPersonPosition::Right,
+                .animation = DialogPersonAnimation::Slide},
+        },
+        {},
+        "Dr. Badut",
+        "Kamu satu-satunya yang bertahan sampai akhir tanpa menjadi bayangan sendiri.!");
+
+    SceneManager_AddDialog(
+        scene->sceneManager,
+        {
+            DialogPerson{
+                .imageFilePath = "HRD/pisangBicara.png",
+                .position = DialogPersonPosition::Right,
+                .animation = DialogPersonAnimation::Slide},
+        },
+        {},
+        "Pisang",
+        "PISAANGGG!", Scene27_PindahScene, scene);
 
     SceneManager_PlayMusic(scene->sceneManager, "Morning.mp3");
 }
--- src/scenes/scene_27/scene_27.hpp
+++ src/scenes/scene_27/scene_27.hpp
@@ -3,6 +3,7 @@
 #include "SFML/Audio.hpp"
 #include "../scene.hpp"
 #include "../scene_28/scene_28.hpp"
+#include "../ending_b/ending_b.hpp"
 #include "../../umum.hpp"
 
 struct Scene27Data
